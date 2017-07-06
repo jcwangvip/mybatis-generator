58785488eeeef9a0fd256bf6dd8768ef97e3aecc
asdc <451809860@qq.com>
2017-07-06 10:28:01 +0800

    s

--- src/com/GeneratorSqlmap.java
+++ src/com/GeneratorSqlmap.java
@@ -22,6 +22,7 @@ public class GeneratorSqlmap {
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,callback, warnings);
         myBatisGenerator.generate(null);
+        
 
     }
 
